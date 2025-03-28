d654d63ca72e16fefa686790abaa7445214276dd
sushka <r.m.poltorabatko@gmail.com>
2025-03-28 13:46:21 +0300

    add errorenum_prepare_app

--- src/fastapi_enum_errors/__init__.py
+++ src/fastapi_enum_errors/__init__.py
@@ -1,6 +1,6 @@
 from fastapi_enum_errors.classproperty import classproperty
 from fastapi_enum_errors.error_enum import ErrorEnum
-from fastapi_enum_errors.handler import jsonhttp_exception_handler
+from fastapi_enum_errors.handler import errorenum_prepare_app
 from fastapi_enum_errors.models import ErrorResponse
 
-__all__ = ["ErrorEnum", "ErrorResponse", "classproperty", "jsonhttp_exception_handler"]
+__all__ = ["ErrorEnum", "ErrorResponse", "classproperty", "errorenum_prepare_app"]
