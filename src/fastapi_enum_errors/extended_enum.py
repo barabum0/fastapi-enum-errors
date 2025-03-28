406d1bb3fcc83e8cb4d46e4d78e8082372141f15
sushka <r.m.poltorabatko@gmail.com>
2025-03-28 13:00:08 +0300

    remove comment

--- src/fastapi_enum_errors/extended_enum.py
+++ src/fastapi_enum_errors/extended_enum.py
@@ -24,25 +24,6 @@ class ExtendedEnum(Enum):
 
         return found_doc.removesuffix('"""').removeprefix('"""')
 
-    @cache
-    def _get_comment(self: Self) -> str | None:
-        sourcelines = [l.strip() for l in inspect.getsourcelines(self.__class__)[0]]
-        search_str = f'{self.name} = "{self.value}"' if isinstance(self.value, str) else f"{self.name} = {self.value}"
-
-        found_str: str | None = next(filter(lambda x: search_str in x, sourcelines), None)  # type: ignore[arg-type, operator]
-        if found_str is None:
-            return None
-
-        found_comment_split = found_str.split("#", 1)
-        if len(found_comment_split) == 1:
-            return None
-
-        return found_comment_split[1].strip()
-
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.__doc__ = self._get_docstring()
-
-    @property
-    def __comment__(self) -> str | None:
-        return self._get_comment()
