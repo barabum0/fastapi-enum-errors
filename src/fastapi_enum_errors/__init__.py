a5a6e23dea351e907df072fbe2827e71d87a0c25
sushka <r.m.poltorabatko@gmail.com>
2025-03-28 13:25:25 +0300

    add classproperty

--- src/fastapi_enum_errors/__init__.py
+++ src/fastapi_enum_errors/__init__.py
@@ -1,4 +1,5 @@
+from fastapi_enum_errors.classproperty import classproperty
 from fastapi_enum_errors.error_enum import ErrorEnum
 from fastapi_enum_errors.models import ErrorResponse
 
-__all__ = ["ErrorEnum", "ErrorResponse"]
+__all__ = ["ErrorEnum", "ErrorResponse", "classproperty"]
